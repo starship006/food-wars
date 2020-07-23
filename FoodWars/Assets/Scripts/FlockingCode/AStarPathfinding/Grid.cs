c92306f7cec4416a1645d28b90668474653c40dc
starship006 <33463067+starship006@users.noreply.github.com>
2020-07-23 17:30:18 -0500

    Pathfinding Foundation + FolderCleanup
    Added grid + node functionality for a* pathfinding which will be added as a weight to the enemy. Still funcitonal :)

---
+++ FoodWars/Assets/Scripts/FlockingCode/AStarPathfinding/Grid.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Transform player;
+
+    public LayerMask unwalkableMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;  //number of nodes within the grid
+    Vector2 worldBottomLeft;
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+
+    }
+
+
+
+    public void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        worldBottomLeft = (Vector2) transform.position - Vector2.right * gridWorldSize.x / 2 - Vector2.up * gridWorldSize.y / 2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector2 worldPoint = worldBottomLeft + Vector2.right * (nodeDiameter * x + nodeRadius) + Vector2.up * (nodeDiameter * y + nodeRadius);
+                bool walkable = !(Physics2D.OverlapCircle(worldPoint, nodeRadius,unwalkableMask));
+
+                grid[x, y] = new Node(walkable, worldPoint);
+
+            }
+        }
+    }
+
+    public Node NodeFromWorldPoint(Vector2 worldPosition)
+    {
+        float percentX = (worldPosition.x -worldBottomLeft.x) / gridWorldSize.x;
+        float percentY = (worldPosition.y -worldBottomLeft.y) / gridWorldSize.y;
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+        return grid[x,y];
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector2(gridWorldSize.x, gridWorldSize.y));
+
+        if(grid != null)
+        {
+            Node playerNode = NodeFromWorldPoint(player.position);
+            foreach (Node n in grid)
+            {
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                if(playerNode == n)
+                {
+                    Gizmos.color = Color.cyan;
+                }
+                Gizmos.DrawCube(n.worldPosition, Vector2.one * (nodeDiameter - 0.1f));
+            }
+        }
+    }
+
+    
+}
---
+++ FoodWars/Assets/Scripts/FlockingCode/BehaviorScripts/AStarChaseBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Flock/Behavior/AStarChase")]
+public class AStarChaseBehavior : FlockBehavior
+{
+        
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+
+
+        return Vector2.zero;
+    }
+}
