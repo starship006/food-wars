3e752bbc1cc790731c2bed7b08059ba46b5525bc
Alavi-Ullah <54385477+DoubleHelix3@users.noreply.github.com>
2020-07-23 12:39:36 -0500

    Added Alavi's code
    Added dungeon generation code:
    Dungeon Layout Generation Skeleton
    Room Shape Generation with the cellular automata algorithm

---
+++ FoodWars/Assets/Scripts/DungeonGeneration/Dungeon Generation.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+
+namespace DungeonGeneration {
+    public static class DungeonLayoutGeneration {
+        public enum RoomTypes {
+            EmptyRoom, Wall, Enemies, Treasure, MiniBoss, Special
+        }
+
+        public struct Map {
+            public RoomTypes[,] rooms;
+
+            public readonly int width;
+            public readonly int height;
+
+            public Map(int width,int height) {
+                this.width = width;
+                this.height = height;
+                this.rooms = new RoomTypes[width,height];
+            }
+        }
+
+        public static void GenerateDungeonLayout(ref Map map, string seed) {
+            DungeonShapeGeneration.GenerateDungeonShape(ref map, seed);
+
+        }
+
+        private static class DungeonShapeGeneration {
+            public static void GenerateDungeonShape(ref Map map, string seed) {
+
+            }
+
+            public static void GenerateGridOfRooms(ref Map map, (int x, int y) location, int size) {
+
+            }
+        }
+
+        // Get the number rooms of certain types in an nxn square centered at some location 
+        static int CountNeighborsOfTypes(in Map map, (int x, int y) location, int distance, params RoomTypes[] roomTypes) {
+            int count = 0;
+            for (int neighbourX = location.x - distance; neighbourX <= location.x + distance; neighbourX++) {
+                for (int neighbourY = location.y - distance; neighbourY <= location.y + distance; neighbourY++) {
+                    if ((neighbourX, neighbourY) == location) continue;
+                    if (neighbourX >= 0 && neighbourX < map.width && neighbourY >= 0 && neighbourY < map.height) {
+                        count += roomTypes.Contains(map.rooms[neighbourX, neighbourY]) ? 1 : 0;
+                    } else {
+                        // Edges are counted as walls
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ FoodWars/Assets/Scripts/DungeonGeneration/ShowRoom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RLG = DungeonGeneration.RoomLayoutGeneration;
+
+[Serializable]
+public class ShowRoom : MonoBehaviour {
+    [SerializeField]
+    GameObject tilePrefab;
+
+    void Start() {
+        int width = 50;
+        int height = 25;
+
+        RLG.Layout room = new RLG.Layout(width, height);
+        RLG.GenerateRoomLayout(ref room, Time.time+"");
+
+        DrawDungeon(in room);
+    }
+
+    void DrawDungeon(in RLG.Layout room) {
+        for (int x = 0; x < room.width; x++) {
+            for (int y = 0; y < room.height; y++) {
+                UnityEngine.Color color = (room.tiles[x, y] == RLG.TileTypes.Wall) ? UnityEngine.Color.black : UnityEngine.Color.white;
+                Vector3 pos = new Vector3(-0.5f * (room.width - 1) + x, -0.5f * (room.height - 1) + y, 0);
+                GameObject roomVisual = Instantiate(tilePrefab, pos, Quaternion.identity);
+                roomVisual.transform.parent = this.transform;
+                roomVisual.GetComponent<SpriteRenderer>().color = color;
+            }
+        }
+    }
+
+    void Update() {
+        // Generate a new room when the  space bar is pressed
+        if (Input.GetMouseButtonDown(0)) Start();
+    }
+
+
+}
